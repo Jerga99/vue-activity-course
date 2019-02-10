6ff68d270866757d0fd7aed856d91ddc8cd4bf60
Filip Jerga <jerga99@gmail.com>
2019-02-10 13:03:20 +0000

    9 Activity Create Component

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
-    'vue/singleline-html-element-content-newline': 0
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/max-attributes-per-line': 0,
+    'vue/html-closing-bracket-newline': 0,
+    'vue/multiline-html-element-content-newline': 0
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -36,71 +36,7 @@
     <section class="container">
       <div class="columns">
         <div class="column is-3">
-          <a
-            v-if="!isFormDisplayed"
-            class="button is-primary is-block is-alt is-large"
-            href="#"
-            @click="toggleFormDisplay"
-          >
-            New Activity
-          </a>
-          <div
-            v-if="isFormDisplayed"
-            class="create-form"
-          >
-            <h2>Create Activity</h2>
-            <form>
-              <div class="field">
-                <label class="label">Title</label>
-                <div class="control">
-                  <input
-                    v-model="newActivity.title"
-                    class="input"
-                    type="text"
-                    placeholder="Read a Book"
-                  >
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Notes</label>
-                <div class="control">
-                  <textarea
-                    v-model="newActivity.notes"
-                    class="textarea"
-                    placeholder="Write some notes here"
-                  />
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Notes</label>
-                <div class="control">
-                  <select v-model="newActivity.category" class="select">
-                    <option disabled value="">Please Select One</option>
-                    <option v-for="category in categories">{{category.text}}</option>
-                  </select>
-                </div>
-              </div>
-              <div class="field is-grouped">
-                <div class="control">
-                  <button
-                    class="button is-link"
-                    :disabled="!isFormValid"
-                    @click="createActivity"
-                  >
-                    Create Activity
-                  </button>
-                </div>
-                <div class="control">
-                  <button
-                    class="button is-text"
-                    @click="toggleFormDisplay"
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+          <ActivityCreate :categories="categories" />
         </div>
         <div class="column is-9">
           <div class="box content">
@@ -120,20 +56,15 @@
 
 <script>
 import ActivityItem from '@/components/ActivityItem'
+import ActivityCreate from '@/components/ActivityCreate'
 import { fetchActivities, fetchUser, fetchCategories } from '@/api'
 export default {
   name: 'App',
-  components: {ActivityItem},
+  components: {ActivityItem, ActivityCreate},
   data () {
     return {
-      isFormDisplayed: false,
       creator: 'Filip Jerga',
       appName: 'Activity Planner',
-      newActivity: {
-        title: '',
-        notes: '',
-        category: ''
-      },
       items: {1: {name: 'Filip'}, 2: {name: 'John'}},
         user: {},
         activities: {},
@@ -141,9 +72,6 @@ export default {
     }
   },
   computed: {
-    isFormValid () {
-      return this.newActivity.title && this.newActivity.notes
-    },
     fullAppName () {
       return this.appName + ' by ' + this.creator
     },
@@ -166,12 +94,6 @@ export default {
     this.categories = fetchCategories()
   },
   methods: {
-    toggleFormDisplay () {
-      this.isFormDisplayed = !this.isFormDisplayed
-    },
-    createActivity () {
-      console.log(this.newActivity)
-    }
   }
 }
 </script>
